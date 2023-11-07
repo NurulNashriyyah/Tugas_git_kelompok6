c77314a212d704b94da3e4d10a8d5367eeb59025
ZahraZafiraa <132476166+ZahraZafiraa@users.noreply.github.com>
2023-11-07 21:50:05 +0700

    Update Kendaraan.java

--- Aplikasi Penghitung Tarif Parkir/Kendaraan.java
+++ Aplikasi Penghitung Tarif Parkir/Kendaraan.java
@@ -1,5 +1,12 @@
+// Kelas Kendaraan (Superclass)
 public class Kendaraan {
-  
-  public Kendaraan {
-  }
+    private String nomorPlat;
+
+    public Kendaraan(String nomorPlat) {
+        this.nomorPlat = nomorPlat;
+    }
+
+    public String getNomorPlat() {
+        return nomorPlat;
+    }
 }
