ac48e44bef04b3d2f177c2b16bab398ece185644
Nurul Uzratun Nashriyyah <130304310+NurulNashriyyah@users.noreply.github.com>
2023-11-07 22:11:24 +0700

    Update Parkir.java
    Menambahkan kode

--- Aplikasi Penghitung Tarif Parkir/Parkir.java
+++ Aplikasi Penghitung Tarif Parkir/Parkir.java
@@ -1,5 +1,20 @@
 public class Parkir {
+  private Kendaraan kendaraan;
+  private int jamParkir;
 
-  public Parkir {
+  public Parkir (Kendaraan kendaraan, int jamParkir) {
+    this.kendaraan = kendaraan;
+    this.jamParkir = jamParkir;
   }
+  public int hitungBiaya() {
+    if (kendaraan instanceof Mobil) {
+      return jamParkir * 5000;  // Biaya parkir mobil per jam
+    } else if (kendaraan instanceof Motor) {
+      return jamParkir * 3000;  // Biaya parkir motor per jam
+    } else if (kendaraan instanceof Bus) {
+      return jamParkir * 10000;  // Biaya parkir bus per jam
+    } else {
+      return 0;
+    }
+   }
 }
