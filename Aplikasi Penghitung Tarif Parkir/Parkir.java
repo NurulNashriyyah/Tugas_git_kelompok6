89c4bcdf1d8f725e9082e738f739d3ca06614bc7
Nurul Uzratun Nashriyyah <130304310+NurulNashriyyah@users.noreply.github.com>
2023-11-11 10:45:37 +0700

    Update Parkir.java
    Menambahkan method masuk dan keluar

--- Aplikasi Penghitung Tarif Parkir/Parkir.java
+++ Aplikasi Penghitung Tarif Parkir/Parkir.java
@@ -1,7 +1,22 @@
-public class Parkir {
-  public Parkir (){
-  }
+// Kelas Parkir
+import java.util.Map;
+import java.util.HashMap;
+import java.time.LocalTime;
 
-  public static void main (String args [] ) {
-  }
+public class Parkir {
+  private Map<Kendaraan, LocalTime> kendaraanParkir = new HashMap<>();
+
+    public void masuk(Kendaraan kendaraan, LocalTime waktuMasuk) {
+        kendaraanParkir.put(kendaraan, waktuMasuk);
+    }
+
+    public LocalTime keluar(Kendaraan kendaraan, LocalTime waktuKeluar) {
+        LocalTime waktuMasuk = kendaraanParkir.get(kendaraan);
+        if (waktuMasuk != null) {
+            kendaraanParkir.remove(kendaraan);
+            return waktuMasuk;
+        } else {
+            return null;
+        }
+    }
 }
