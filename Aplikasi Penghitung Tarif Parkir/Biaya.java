74467daafff5c357619383510631625a9a6d6847
Nurul Uzratun Nashriyyah <130304310+NurulNashriyyah@users.noreply.github.com>
2023-11-11 10:54:53 +0700

    Update Biaya.java

--- Aplikasi Penghitung Tarif Parkir/Biaya.java
+++ Aplikasi Penghitung Tarif Parkir/Biaya.java
@@ -1,20 +1,25 @@
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Biaya {
   private Kendaraan kendaraan;
-  private int jamParkir;
+  private long jamParkir;
 
-  public Biaya (Kendaraan kendaraan, int jamParkir) {
-    this.kendaraan = kendaraan;
-    this.jamParkir = jamParkir;
+  public Biaya (Kendaraan kendaraan, LocalTime waktuMasuk, LocalTime waktuKeluar) {
+        this.kendaraan = kendaraan;
+        Duration durasi = Duration.between(waktuMasuk, waktuKeluar);
+        this.jamParkir = durasi.toHours(); // Menghitung durasi parkir dalam jam
   }
   public int hitungBiaya() {
-    if (kendaraan instanceof Mobil) {
-      return jamParkir * 5000;  // Biaya parkir mobil per jam
-    } else if (kendaraan instanceof Motor) {
-      return jamParkir * 3000;  // Biaya parkir motor per jam
-    } else if (kendaraan instanceof Bus) {
-      return jamParkir * 10000;  // Biaya parkir bus per jam
-    } else {
-      return 0;
-    }
-   }
+    String jenis = kendaraan.getJenis();
+        if (jenis.equals("Mobil")) {
+            return (int)(jamParkir + 1) * 3000;  // Biaya parkir mobil per jam
+        } else if (jenis.equals("Motor")) {
+            return (int)(jamParkir + 1) * 2000;  // Biaya parkir motor per jam
+        } else if (jenis.equals("Bus")) {
+            return (int)(jamParkir + 1) * 5000;  // Biaya parkir bus per jam
+        } else {
+            return 0;
+        }
+  }
 }
