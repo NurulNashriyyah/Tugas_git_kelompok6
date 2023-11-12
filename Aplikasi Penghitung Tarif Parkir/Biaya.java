54d09f1deac981c2aa6f5ab80d71cd5ca99a56b6
ZahraZafiraa <132476166+ZahraZafiraa@users.noreply.github.com>
2023-11-12 23:24:22 +0700

    Update Biaya.java

--- Aplikasi Penghitung Tarif Parkir/Biaya.java
+++ Aplikasi Penghitung Tarif Parkir/Biaya.java
@@ -1,31 +1,52 @@
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Biaya {
-  private Kendaraan kendaraan;
-  private long jamParkir;
+    private Kendaraan kendaraan;
+    private long menitParkir;
 
-  public Biaya(Kendaraan kendaraan, LocalTime waktuMasuk, LocalTime waktuKeluar) {
+    public Biaya(Kendaraan kendaraan, LocalTime waktuMasuk, LocalTime waktuKeluar) {
         this.kendaraan = kendaraan;
-        Duration durasi = Duration.between(waktuMasuk, waktuKeluar);
-        this.jamParkir = durasi.toMinutes(); // Menghitung durasi parkir dalam menit
+
+        if (waktuKeluar.isBefore(waktuMasuk)) {
+            // Kendaraan keluar pada hari berikutnya
+            Duration durasiHariIni = Duration.between(waktuMasuk, LocalTime.of(23, 59));
+            Duration durasiHariBesok = Duration.between(LocalTime.of(0, 0), waktuKeluar);
+            this.menitParkir = durasiHariIni.toMinutes() + durasiHariBesok.toMinutes();
+        } else {
+            // Kendaraan keluar pada hari yang sama
+            Duration durasi = Duration.between(waktuMasuk, waktuKeluar);
+            this.menitParkir = durasi.toMinutes();
+        }
     }
 
     public int hitungBiaya() {
         String jenis = kendaraan.getJenis();
-        int biayaPerJam;
+        int biayaPerJam = 0; // Biaya parkir per jam
+        int menitPerJam = 60; // Menit dalam satu jam
 
         if (jenis.equals("Mobil")) {
-            biayaPerJam = 3000;  // Biaya parkir mobil per jam
+            biayaPerJam = 3000; // Biaya parkir mobil per jam
         } else if (jenis.equals("Motor")) {
-            biayaPerJam = 2000;  // Biaya parkir motor per jam
+            biayaPerJam = 2000; // Biaya parkir motor per jam
         } else if (jenis.equals("Bus")) {
-            biayaPerJam = 5000;  // Biaya parkir bus per jam
+            biayaPerJam = 5000; // Biaya parkir bus per jam
         } else {
-            return 0; // Kendaraan tidak valid
+            return 0;
         }
 
-        int biayaTotal = (int) Math.ceil((double) jamParkir / 60) * biayaPerJam; // Biaya dibulatkan ke atas per jam
-        return biayaTotal;
+        int biaya = 0;
+
+        if (menitParkir <= menitPerJam) {
+            // Kendaraan parkir kurang dari satu jam
+            biaya = biayaPerJam;
+        } else {
+            // Kendaraan parkir lebih dari satu jam
+            double jamPenuh = Math.ceil((double) menitParkir / menitPerJam);
+            biaya = (int) jamPenuh * biayaPerJam;
+        }
+
+        return biaya;
     }
 }
